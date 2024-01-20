aae0c8cb1c1ee42534d48eb2c5990040be839f82
Jorio <jjrzx@outlook.com>
2024-01-19 21:33:16 -0500

    Updates

--- ascii.js
+++
@@ -1,5 +0,0 @@
-{/* prettier-ignore */}
-const asciiArt =
-  '\n        88                           88\n        88                           ""\n        88\n        88   ,adPPYba,   8b,dPPYba,  88   ,adPPYba,\n        88  a8"     "8a  88P\'   "Y8  88  a8"     "8a\n        88  8b       d8  88          88  8b       d8\n88,   ,d88  "8a,   ,a8"  88          88  "8a,   ,a8"\n "Y8888P"    `"YbbdP"\'   88          88   `"YbbdP"\'';
-
-export default asciiArt;
